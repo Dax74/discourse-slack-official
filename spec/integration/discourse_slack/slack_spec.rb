f40d6e9a35203bfe87cb23588c6b7707ced18092
Vinoth Kannan <vinothkannan@vinkas.com>
2017-03-18 18:23:00 +0530

    FEATURE: Add ability to watch tags.

--- spec/integration/discourse_slack/slack_spec.rb
+++ spec/integration/discourse_slack/slack_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 describe 'Slack' do
+  let(:first_post) { Fabricate(:post) }
+  let(:topic) { Fabricate(:topic, posts: [first_post]) }
+  let(:delay) { SiteSetting.post_to_slack_window_secs.seconds }
+
   before do
     SiteSetting.slack_outbound_webhook_url = "https://hooks.slack.com/services/abcde"
     SiteSetting.slack_enabled = true
     SiteSetting.post_to_slack_window_secs = 20
   end
 
-  let(:first_post) { Fabricate(:post) }
-  let(:topic) { Fabricate(:topic, posts: [first_post]) }
-  let(:delay) { SiteSetting.post_to_slack_window_secs.seconds }
-
   describe 'testing notification' do
     it 'should ping slack successfully' do
-      DiscourseSlack::Slack.expects(:notify).with(first_post.id)
+      DiscourseSlack::Slack.expects(:notify)
 
       post '/slack/test.json'
 
@@ -22,7 +22,6 @@ describe 'Slack' do
   end
 
   context 'post' do
-
     it 'should schedule a job for slack post' do
       Timecop.freeze(Time.zone.now) do
         Jobs.expects(:enqueue_in).with(delay, :notify_slack, has_entry(post_id: first_post.id))
@@ -42,7 +41,5 @@ describe 'Slack' do
         end
       end
     end
-
   end
-
 end
